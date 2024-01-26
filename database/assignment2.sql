59608bb660a01fad53b0f4bec931c12194a399aa
CabrynBray <cabrynbrayza@gmail.comgit config --global user.name>
2024-01-26 15:33:23 +0200

    removed one image and vehicles

--- database/assignment2.sql
+++ database/assignment2.sql
@@ -40,6 +40,6 @@ WHERE classification.classification_name = 'Sport';
 -- Update all records in the inventory table to add "/vehicles" to the middle of the file
 -- path in the inv_image and inv_thumbnail columns using a single query.
 -- When done the path for both inv_image and inv_thumbnail should resemble this example: /images/vehicles/a-car-name.jpg
-UPDATE public.inventory
-SET inv_image = REPLACE(inv_image, '/images', '/images/vehicles'),
-    inv_thumbnail = REPLACE(inv_thumbnail, '/images', '/images/vehicles');
+-- UPDATE public.inventory
+-- SET inv_image = REPLACE(inv_image, '/images', '/images/vehicles'),
+--     inv_thumbnail = REPLACE(inv_thumbnail, '/images', '/images/vehicles');
